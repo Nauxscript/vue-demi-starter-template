74dfebfd3cb1ef6501e21b29bc93746fdda9fcc9
nauxscript <nauxscript@gmail.com>
2022-08-18 11:26:01 +0800

    :sparkles: updated components

--- packages/components/button/src/Button.vue
+++ packages/components/button/src/Button.vue
@@ -2,8 +2,10 @@
 import { defineComponent, isVue2, h } from 'vue-demi'
 
 export default defineComponent({
+  name: 'FunButton',
   render(h_vue2) {
     if (isVue2) {
+      console.log(h_vue2)
       return h_vue2("p", {}, 'Vue 2 Button For Fun 💖')
     }
     return h('p', {}, 'Vue 3 Button For Fun ✨')
