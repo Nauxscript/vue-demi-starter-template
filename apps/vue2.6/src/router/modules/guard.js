af67770cfa158d7ba4df184e183b6b7d92f0d09d
nauxscript <nauxscript@gmail.com>
2022-10-14 10:25:59 +0800

    :fire: delete demo projects

--- apps/vue2.6/src/router/modules/guard.js
+++
@@ -1,9 +0,0 @@
-import router from "..";
-/**
- * Vue-router Navigation Guard
- */
-//External Route Guard Before Routing
-router.beforeEach((to, from, next) => { })
-
-//External Route Guard After Routing
-router.afterEach((to, from) => { })
